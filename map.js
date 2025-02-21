5621e4186e0d0fa3af7c73cb5fd145f929ecbc04
danielbirman28 <dbirman28@gmail.com>
2025-02-20 19:46:06 -0800

    Step 2: Adding bike lanes

--- map.js
+++ map.js
@@ -6,9 +6,44 @@ document.addEventListener('DOMContentLoaded', () => {
     const map = new mapboxgl.Map({
         container: 'map', // ID of the div where the map will render
         style: 'mapbox://styles/mapbox/streets-v12', // Map style
+        // style: 'mapbox://styles/mapbox/light-v11', // Map style
         center: [-71.09415, 42.36027], // [longitude, latitude]
         zoom: 12, // Initial zoom level
         minZoom: 5, // Minimum allowed zoom
         maxZoom: 18  // Maximum allowed zoom
     });
+
+    map.on('load', () => { 
+        map.addSource('boston_route', {
+            type: 'geojson',
+            data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson?...'
+        });
+
+        map.addSource('cambridge_route', {
+            type: 'geojson',
+            data: 'https://raw.githubusercontent.com/cambridgegis/cambridgegis_data/main/Recreation/Bike_Facilities/RECREATION_BikeFacilities.geojson'
+        });
+        
+        map.addLayer({
+            id: 'bike-lanes-boston',
+            type: 'line',
+            source: 'boston_route',
+            paint: {
+              'line-color': 'green',
+              'line-width': 3,
+              'line-opacity': 0.4
+            }
+          });
+
+          map.addLayer({
+            id: 'bike-lanes-cambridge',
+            type: 'line',
+            source: 'cambridge_route',
+            paint: {
+              'line-color': 'green',
+              'line-width': 3,
+              'line-opacity': 0.4
+            }
+          });
+      });
 });
